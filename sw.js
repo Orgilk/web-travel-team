b91a48168cebafc0b0fdb87afaecfb5ff4f6923a
Orgilk <127022751+Orgilk@users.noreply.github.com>
2025-01-01 00:44:32 +0800

    qqqq

--- sw.js
+++ sw.js
@@ -1,40 +1,40 @@
-const cacheName = 'pwa-display-test-v5';
-const contentToCache = [
-  '/{display_mode}/',
-  '/{display_mode}/index.html',
-  '/{display_mode}/destinations.html',
-  '/{display_mode}/about.html',
-  '/{display_mode}/news.html',
-  '/{display_mode}/Altai.html',
-  '/{display_mode}/css/styles.css',
-];
+const myCache = 'WEB_APP_CACHE_NAME';
 
-// Install the Service Worker
-self.addEventListener('install', (e) => {
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(contentToCache);
-  })());
+self.addEventListener('install', (event) => {
+    event.waitUntil(async function() {
+        const cache = await caches.open(myCache);
+        await cache.addAll([
+            '/'
+        ]);
+    }());
 });
 
-// Fetch content using the Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
+self.addEventListener('activate', (event) => {
+    event.waitUntil(async function() {
+        const cache = await caches.open(myCache);
+        const cacheNames = await cache.keys();
+        await Promise.all(
+            cacheNames.map(cacheName => caches.delete(cacheName))
+        );
+    }());
 });
 
-// Clear the cache
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key === cacheName) { return; }
-      return caches.delete(key);
-    }))
-  }));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(async function() {
+        const networkPromise = fetch(event.request);
+        const cachePromise = caches.open(myCache);
+
+        try {
+            const networkResponse = await networkPromise;
+            cachePromise
+                .then(cache => {
+                    cache.put(event.request, networkResponse);
+                });
+            return networkResponse.clone();
+        } catch (error) {
+            const cache = await cachePromise;
+            const cacheResponse = await cache.match(event.request);
+            return cacheResponse;
+        }
+    }());
 });
