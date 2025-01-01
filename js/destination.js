0900d5f70b94dfbb03aa02ce2035530395365838
Orgilk <127022751+Orgilk@users.noreply.github.com>
2025-01-01 19:59:09 +0800

    qqqqq

--- js/destination.js
+++ js/destination.js
@@ -133,15 +133,16 @@ const renderArticles = async (filterRegion = 'All', searchName = '', searchPlace
                     <p>Үнэ: <span class="price">${item.price}</span></p>
                 </div>
                 <div class="flip-card-back">
-                    <h4>${item.name}</h4>
-                    <p>${item.description}</p>
-                    <p>Region: ${item.region}</p>
-                    <a href="${item.link}" class="learn-more">Цааш үзэх</a>
-                    <div class="favorite" data-id="${item.id}">
+                 <div class="favorite" data-id="${item.id}">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="70" height="30">
                             <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="none" stroke="currentColor" stroke-width="2"/>
                         </svg>
                     </div>
+                      <h4>${item.name}</h4>
+                    <p>${item.description}</p>
+                    <p>Region: ${item.region}</p>
+                    <a href="${item.link}" class="learn-more">Цааш үзэх</a>
+                   
                 </div>
             </div>
         `;
