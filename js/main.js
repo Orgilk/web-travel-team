ae4298f0b129ae63dd5b0be3ec209908b8aa0a9f
Orgilk <127022751+Orgilk@users.noreply.github.com>
2025-01-06 12:38:08 +0800

    qqqqq

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { TravelFilter } from "./travelFilterModule.js";
-
+// Dom achaalagdsan ued class-iig ehluulne
 document.addEventListener("DOMContentLoaded", () => {
     new TravelFilter();
 });
